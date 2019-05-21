3316bf449acfe7768bf7fbf0c8d91e0cef148573
Sebastien Van Cauwenberghe <svancau@gmail.com>
2019-05-21 19:31:17 +0200

    Add translation of VHDL module

--- src/V4VhdlTranslate.cpp
+++ src/V4VhdlTranslate.cpp
@@ -2,6 +2,17 @@
 #include "V3FileLine.h"
 #include "V4VhdlTranslate.h"
 
+#define VARRESET_LIST(decl)    { pinnum=1; VARRESET(); VARDECL(decl); }	// Start of pinlist
+#define VARRESET_NONLIST(decl) { pinnum=0; VARRESET(); VARDECL(decl); }	// Not in a pinlist
+#define VARRESET() { VARDECL(UNKNOWN); VARIO(NONE); VARDTYPE(NULL); }
+#define VARDECL(type) { m_varDecl = AstVarType::type; }
+#define VARIO(type) { m_varIO = VDirection::type; }
+#define VARDTYPE(dtypep) { m_varDTypep = dtypep; }
+
+#define VARDONEA(fl,name,array,attrs) GRAMMARP->createVariable((fl),(name),(array),(attrs))
+#define VARDONEP(portp,array,attrs) GRAMMARP->createVariable((portp)->fileline(),(portp)->name(),(array),(attrs))
+#define PINNUMINC() (pinnum++)
+
 V4VhdlTranslate::V4VhdlTranslate(V3ParseSym &symtable) : symt(symtable)
 {
 
@@ -10,6 +21,108 @@ V4VhdlTranslate::V4VhdlTranslate(V3ParseSym &symtable) : symt(symtable)
 V4VhdlTranslate::~V4VhdlTranslate()
 {
 
+}
+
+AstNodeDType* V4VhdlTranslate::createArray(AstNodeDType* basep, AstNodeRange* nrangep, bool isPacked) {
+    // Split RANGE0-RANGE1-RANGE2 into ARRAYDTYPE0(ARRAYDTYPE1(ARRAYDTYPE2(BASICTYPE3),RANGE),RANGE)
+    AstNodeDType* arrayp = basep;
+    if (nrangep) { // Maybe no range - return unmodified base type
+        while (nrangep->nextp()) nrangep = VN_CAST(nrangep->nextp(), NodeRange);
+        while (nrangep) {
+            AstNodeRange* prevp = VN_CAST(nrangep->backp(), NodeRange);
+            if (prevp) nrangep->unlinkFrBack();
+            AstRange* rangep = VN_CAST(nrangep, Range);
+            if (!rangep) {
+                if (!VN_IS(nrangep, UnsizedRange)) nrangep->v3fatalSrc("Expected range or unsized range");
+                arrayp = new AstUnsizedArrayDType(nrangep->fileline(), VFlagChildDType(), arrayp);
+            } else if (isPacked) {
+                arrayp = new AstPackArrayDType(rangep->fileline(), VFlagChildDType(), arrayp, rangep);
+            } else {
+                arrayp = new AstUnpackArrayDType(rangep->fileline(), VFlagChildDType(), arrayp, rangep);
+            }
+            nrangep = prevp;
+        }
+    }
+    return arrayp;
+}
+
+AstVar* V4VhdlTranslate::createVariable(FileLine* fileline, string name, AstNodeRange* arrayp, AstNode* attrsp) {
+    AstNodeDType* dtypep = m_varDTypep;
+    //UINFO(5,"  creVar "<<name<<"  decl="<<GRAMMARP->m_varDecl<<"  io="<<GRAMMARP->m_varIO<<"  dt="<<(dtypep?"set":"")<<endl);
+    if (m_varIO == VDirection::NONE
+	&& m_varDecl == AstVarType::PORT) {
+	// Just a port list with variable name (not v2k format); AstPort already created
+	if (dtypep) fileline->v3error("Unsupported: Ranges ignored in port-lists");
+	return NULL;
+    }
+    if (m_varDecl == AstVarType::WREAL) {
+	// dtypep might not be null, might be implicit LOGIC before we knew better
+	dtypep = new AstBasicDType(fileline,AstBasicDTypeKwd::DOUBLE);
+    }
+    if (!dtypep) {  // Created implicitly
+	dtypep = new AstBasicDType(fileline, AstBasicDTypeKwd::LOGIC_IMPLICIT);
+    } else {  // May make new variables with same type, so clone
+	dtypep = dtypep->cloneTree(false);
+    }
+    //UINFO(0,"CREVAR "<<fileline->ascii()<<" decl="<<GRAMMARP->m_varDecl.ascii()<<" io="<<GRAMMARP->m_varIO.ascii()<<endl);
+    AstVarType type = m_varDecl;
+    if (type == AstVarType::UNKNOWN) {
+        if (m_varIO.isAny()) {
+            type = AstVarType::PORT;
+        } else {
+            fileline->v3fatalSrc("Unknown signal type declared");
+        }
+    }
+    if (type == AstVarType::GENVAR) {
+	if (arrayp) fileline->v3error("Genvars may not be arrayed: "<<name);
+    }
+
+    // Split RANGE0-RANGE1-RANGE2 into ARRAYDTYPE0(ARRAYDTYPE1(ARRAYDTYPE2(BASICTYPE3),RANGE),RANGE)
+    AstNodeDType* arrayDTypep = createArray(dtypep, arrayp, false);
+
+    AstVar* nodep = new AstVar(fileline, type, name, VFlagChildDType(), arrayDTypep);
+    nodep->addAttrsp(attrsp);
+    if (m_varDecl != AstVarType::UNKNOWN) nodep->combineType(m_varDecl);
+    if (m_varIO != VDirection::NONE) {
+        nodep->declDirection(m_varIO);
+        nodep->direction(m_varIO);
+    }
+
+    if (m_varDecl == AstVarType::SUPPLY0) {
+	nodep->addNext(createSupplyExpr(fileline, nodep->name(), 0));
+    }
+    if (m_varDecl == AstVarType::SUPPLY1) {
+	nodep->addNext(createSupplyExpr(fileline, nodep->name(), 1));
+    }
+    if (VN_IS(dtypep, ParseTypeDType)) {
+	// Parser needs to know what is a type
+	AstNode* newp = new AstTypedefFwd(fileline, name);
+	nodep->addNext(newp);
+	symt.reinsert(newp);
+    }
+    // Don't set dtypep in the ranging;
+    // We need to autosize parameters and integers separately
+    //
+    // Propagate from current module tracing state
+    if (nodep->isGenVar()) nodep->trace(false);
+    else if (nodep->isParam() && !v3Global.opt.traceParams()) nodep->trace(false);
+    else nodep->trace(allTracingOn(nodep->fileline()));
+
+    // Remember the last variable created, so we can attach attributes to it in later parsing
+    //m_varAttrp = nodep;
+    //tagNodep(m_varAttrp);
+    return nodep;
+}
+
+AstNode* V4VhdlTranslate::createSupplyExpr(FileLine* fileline, string name, int value) {
+    FileLine* newfl = new FileLine(fileline);
+    newfl->warnOff(V3ErrorCode::WIDTH, true);
+    AstNode* nodep = new AstConst(newfl, V3Number(newfl));
+    // Adding a NOT is less work than figuring out how wide to make it
+    if (value) nodep = new AstNot(newfl, nodep);
+    nodep = new AstAssignW(newfl, new AstVarRef(fileline, name, true),
+			   nodep);
+    return nodep;
 }
 
 AstNodeDType *V4VhdlTranslate::translateType(Value::ConstObject item) {
@@ -48,31 +161,47 @@ AstNode *V4VhdlTranslate::translateFcall(Value::ConstObject item) {
     return nullptr;
 }
 
+string convertName(string inName) {
+    return inName.erase(0,5);
+}
+
 AstNode *V4VhdlTranslate::translateObject(Value::ConstObject item) {
     auto obj = item;
     if (obj["cls"] == "entity") {
         FileLine *fl = new FileLine("", 0);
-        AstModule *mod = new AstModule(fl, obj["name"].GetString());
+        string module_name = convertName(obj["name"].GetString());
+        AstModule *mod = new AstModule(fl, module_name);
 
         // Handle Ports
         pinnum = 0;
         auto port_array = obj["port"].GetArray();
         for(Value::ConstValueIterator m = port_array.Begin(); m != port_array.End(); ++m) {
             auto port_obj = m->GetObject();
+            cout << port_obj["dir"].GetString() << endl;
+            string direction = port_obj["dir"].GetString();
+            if (direction == "in") {
+                cout << "in"<<endl;
+                VARIO(INPUT);
+            } else if (direction == "out") {
+                cout << "out"<<endl;
+                VARIO(OUTPUT);
+            } else if (direction == "inout") {
+                cout << "inout"<<endl;
+                VARIO(INOUT);
+            }
+            VARDECL(PORT);
             FileLine *fl = new FileLine("", 0);
             AstPort *port = new AstPort(fl, pinnum++, port_obj["name"].GetString());
-            mod->addStmtp(port);
             FileLine *fl2 = new FileLine("", 0);
-            AstBasicDType *dtypep = new AstBasicDType(fl2, AstBasicDTypeKwd::BIT);
-            cout << dtypep << endl;
-            FileLine *fl3 = new FileLine("", 0);
-            AstVar* var = new AstVar(fl3, AstVarType::PORT, port_obj["name"].GetString(), dtypep);
-            mod->addStmtp(var);
+            VARDTYPE(new AstBasicDType(fl2, AstBasicDTypeKwd::LOGIC_IMPLICIT));
+            mod->addStmtp(port);
+            mod->addStmtp(createVariable(fl, port->name(), NULL, NULL));
         }
+        pinnum = 0;
+
         v3Global.rootp()->addModulep(mod);
         symt.pushNew(mod);
-        m_entities.insert(pair<string,AstNode*>(obj["name"].GetString(), mod));
-
+        m_entities.insert(pair<string,AstNode*>(module_name, mod));
 
     } else if (obj["cls"] == "architecture") {
         auto entity_mod = m_entities.find(obj["of"].GetString());
--- src/V4VhdlTranslate.h
+++ src/V4VhdlTranslate.h
@@ -19,6 +19,17 @@ class V4VhdlTranslate
 {
 private:
     /* data */
+    AstNodeDType* createArray(AstNodeDType* basep, AstNodeRange* rangep, bool isPacked);
+    AstVar*  createVariable(FileLine* fileline, string name, AstNodeRange* arrayp, AstNode* attrsp);
+    AstNode* createSupplyExpr(FileLine* fileline, string name, int value);
+    AstNodeDType* m_varDTypep;	// Pointer to data type for next signal declaration
+    AstVarType	m_varDecl;	// Type for next signal declaration (reg/wire/etc)
+    VDirection  m_varIO;        // Direction for next signal declaration (reg/wire/etc)
+    AstVar*	m_varAttrp;	// Current variable for attribute adding
+    bool allTracingOn(FileLine* fl) {
+	return v3Global.opt.trace() && fl->tracingOn();
+    }
+
     unsigned long pinnum;
     void iterateArray(Value::ConstArray arr, void(*add)(AstNode *newp));
     AstNodeDType *translateType(Value::ConstObject item);
